eef603531dca8d9c066946cf335ccc88cf65efb5
rrus <RandyHR2000@gmail.com>
2019-10-10 18:20:22 -0700

    Added frog
    made frog move

--- GameDevTeam6-Game/Assets/Scripts/TestScript.cs
+++ GameDevTeam6-Game/Assets/Scripts/TestScript.cs
@@ -7,12 +7,18 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int x = 1;
+        float y = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool y = Input.GetKey("w");
+
+        if (y == true)
+        {
+            transform.position = new Vector3(transform.position.x + 0.1f, 0, 0);
+        }
     }
 }
