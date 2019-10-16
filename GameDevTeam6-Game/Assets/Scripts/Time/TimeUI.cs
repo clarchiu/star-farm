56def5faf1b90391e563cd00073205c629eeec83
rrus <RandyHR2000@gmail.com>
2019-10-15 19:42:24 -0700

    Added time system
    Added time with hours and minutes, and seconds.
    Added simple UI for it.

---
+++ GameDevTeam6-Game/Assets/Scripts/Time/TimeUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUI : MonoBehaviour
+{
+    public Text timeText;
+    private TimeSystem system;
+
+    private void Start()
+    {
+        system = GetComponent<TimeSystem>();
+    }
+
+    void Update()
+    {
+        int hour = system.getHour();
+        int minute = system.getMinute();
+        float seconds = system.getSeconds();
+
+        string h = hour.ToString();
+        string m = minute.ToString();
+
+        if (hour < 10) {
+            h = "0" + h.ToString();
+        }
+        if (minute < 10)
+        {
+            m = "0" + m.ToString();
+        }
+
+        timeText.text = "TIME: " + h + " : " + m;
+    }
+}
