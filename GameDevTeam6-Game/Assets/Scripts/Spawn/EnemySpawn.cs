27ca1e61308b10f9841a4be6221727264e9e66e8
clarence <clarchiu@gmail.com>
2019-10-17 19:41:40 -0700

    added enemy spawner to scene

--- GameDevTeam6-Game/Assets/Scripts/Spawn/EnemySpawn.cs
+++ GameDevTeam6-Game/Assets/Scripts/Spawn/EnemySpawn.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour
+public class EnemySpawn : MonoBehaviour
 {
+	public GameObject enemy;
+
     // Start is called before the first frame update
-	public GameObject fly;
-	public 
-
-
     void Start()
     {
-        InvokeRepeating("spawnFly", 2.0f, 1.0f);	//spawn fly every second starting at 2 seconds
+        InvokeRepeating("spawnEnemy", 2.0f, 1.0f);	//spawn fly every second starting at 2 seconds
     }
 
     // Update is called once per frame
@@ -20,31 +18,22 @@ public class Spawn : MonoBehaviour
         
     }
 
-    void spawnFly() 
+    private void spawnEnemy() 
     {
-
     	Vector3 positionAlongEdge = new Vector3(0,0,0);
 
 		int randomEdge = Random.Range(0,4);   //choose a random edge
         float randomPos = Random.Range(0.0f, 1.0f); //choose a random position along the edge
 
-		if (randomEdge == 0) //top Edge
-		{
+		if (randomEdge == 0)  //top edge
         	positionAlongEdge = Camera.main.ViewportToWorldPoint(new Vector3(randomPos, 1, Camera.main.nearClipPlane));
-		}
 		else if (randomEdge == 1) //bottom Edge
-		{
         	positionAlongEdge = Camera.main.ViewportToWorldPoint(new Vector3(randomPos, 0, Camera.main.nearClipPlane));
-		}
 		else if (randomEdge == 2) //left Edge
-		{
        		positionAlongEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, randomPos, Camera.main.nearClipPlane));
-		}
 		else if (randomEdge == 3) //right Edge
-		{
         	positionAlongEdge = Camera.main.ViewportToWorldPoint(new Vector3(1, randomPos, Camera.main.nearClipPlane));
-		}
 
-    	GameObject newFly = Instantiate(fly, positionAlongEdge, Quaternion.identity);
+    	GameObject newEnemy = Instantiate(enemy, positionAlongEdge, Quaternion.identity);
     }
 }
