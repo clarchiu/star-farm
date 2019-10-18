61760b8f54a84553405d11301838f87d8b66539c
rrus <RandyHR2000@gmail.com>
2019-10-17 19:08:57 -0700

    TileMap
    Added tileMap

---
+++ GameDevTeam6-Game/Assets/Scripts/TileMap/TileInformation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInformation : MonoBehaviour
+{
+    public int tileCountX;
+    public int tileCountY;
+    Tile[,] tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = new Tile[20,20];
+        for (int i = 0; i < 20; i ++){
+            for (int j = 0; j < 20; j++) {
+                tiles[i, j] = new Tile();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
