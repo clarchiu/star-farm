4a6b1f135f5f2216464f5513f002316dc05e34e5
Peakachu <41412565+Peakachu@users.noreply.github.com>
2019-10-31 18:35:59 -0700

    fix error

--- GameDevTeam6-Game/Assets/Scripts/Plant/PlantGrowth.cs
+++ GameDevTeam6-Game/Assets/Scripts/Plant/PlantGrowth.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlantGrowth : MonoBehaviour
 {
     public int stages = 0;
+    public int countGrow = 0;
+    public int countFruit = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,16 +17,22 @@ public class PlantGrowth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int countSec = 0;
-        countSec++;
+        countGrow++;
+        countFruit++;
 
-        if (countSec == 360) //stages change every 6 min -- plant grows completely in 30 min
+        if (countGrow == 360) //stage changes every 6 min -- plant grows completely in 30 min
         {
             if (stages <= 5)
             {
                 stages++;
-                countSec -= 360;
+                countGrow -= 360;
             }
+        }
+
+        if (countFruit == 480) //fruit grows every 8 min
+        {
+            //spawn fruit if fruit != maxFruit
+            countFruit -= 480;
         }
     }
 }
