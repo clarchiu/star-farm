2a596baa870fba93fc8c9f83314bab8a6dd2f1d1
TheAvi123 <44722892+TheAvi123@users.noreply.github.com>
2019-11-19 19:42:24 -0800

    Projectile Spawner Basis (Incomplete)

---
+++ GameDevTeam6-Game/Assets/Scripts/Defense/ProjectileSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] private Projectile bullet = null;
+
+    public Projectile SpawnProjectile(Vector3 position, Quaternion rotation) {
+
+        Projectile projectile = Instantiate(bullet, position, rotation) as Projectile;
+        return projectile;
+    }
+}
