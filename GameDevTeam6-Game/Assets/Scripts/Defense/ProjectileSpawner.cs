a2e6d375f94636b9bceecbba684ff1fdcc0b41ee
TheAvi123 <44722892+TheAvi123@users.noreply.github.com>
2019-11-21 19:59:51 -0800

    Projectile Mechanics Complete

--- GameDevTeam6-Game/Assets/Scripts/Defense/ProjectileSpawner.cs
+++ GameDevTeam6-Game/Assets/Scripts/Defense/ProjectileSpawner.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class ProjectileSpawner : MonoBehaviour
 {
+    [SerializeField] bool shootProjectiles = false;
+    private bool shooting = false;
+    [SerializeField] float rateOfFire = 0.5f;
+    private Coroutine shootCoroutine = null;
     [SerializeField] private Projectile bullet = null;
+
+    void Update(){
+        if (shootProjectiles && !shooting) {
+            shootCoroutine = StartCoroutine(ShootCoroutine());
+        }
+        if (!shootProjectiles) {
+            StopAllCoroutines();
+            shooting = false;
+        }
+    }
+
+    IEnumerator ShootCoroutine() {
+        shooting = true;
+        while (shootProjectiles) {
+            SpawnProjectile(gameObject.transform.position, gameObject.transform.eulerAngles);
+            yield return new WaitForSeconds(rateOfFire);
+        }
+        shooting = false;
+    }
 
     public Projectile SpawnProjectile(Vector3 position, Vector3 rotation) {
 
