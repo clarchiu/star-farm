787cd94658e0a76399bc04b557daacab4ec557eb
Claude-lu <49216508+Claude-lu@users.noreply.github.com>
2019-11-28 19:18:44 -0800

    MusicPlayer

---
+++ GameDevTeam6-Game/Assets/Scripts/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+     AudioSource MpPlayer;
+     [SerializeField] AudioClip NightClip = null;
+     [SerializeField] AudioClip NightClip = null;
+     // Use this for initialization
+     void Start () {
+         MpPlayer.clip = FirstClip;
+         MpPlayer.loop = false;
+         MpPlayer.Play();
+         StartCoroutine(WaitForTrackTOend());
+     }
+
+     void Awake() {
+         MpPlayer = gameObject.AddComponent<AudioSource>() as AudioSource;
+     }
+ 
+     IEnumerator WaitForTrackTOend()
+     {
+         while (MpPlayer.isPlaying)
+         {
+             yield return new WaitForSeconds(0.01f);
+             
+         }
+         MpPlayer.clip = FirstClip;
+         MpPlayer.loop = true ;
+         MpPlayer.Play();
+        
+     }
+
+}
