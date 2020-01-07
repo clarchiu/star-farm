07575e97c9dd9de95fe3a2dfa34312477c806de8
rrus <RandyHR2000@gmail.com>
2020-01-06 21:43:33 -0800

    Updated map, added mode wheel

--- GameDevTeam6-Game/Assets/Scripts/MultiTool/MultiTool.cs
+++ GameDevTeam6-Game/Assets/Scripts/MultiTool/MultiTool.cs
@@ -1,46 +1,130 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultiTool : MonoBehaviour
 {
-    ToolModes mode;
+    private ToolModes mode;
     int index;
+
+    private MultiTool multiTool;
+    private GameObject wheel;
+    private Image topLeft;
+    private Image topRight;
+    private Image bottomLeft;
+    private Image bottomRight;
+    private Image currentImage;
+    private Image selectedImage;
+
+    private Color32 blue;
+    private Color32 darkBlue;
+    private Color32 white;
+
 
     private void Start() {
         mode = ToolModes.defaultMode;
-    }
 
-    private void Update() {
-        if (Input.GetKeyDown("f")) {
-            SwitchTool();
-        }
-    }
+        multiTool = FindObjectOfType<MultiTool>();
+        wheel = GameObject.FindWithTag("wheel");
+        topLeft = wheel.transform.Find("TopLeft").GetComponent<Image>();
+        topRight = wheel.transform.Find("TopRight").GetComponent<Image>();
+        bottomLeft = wheel.transform.Find("BottomLeft").GetComponent<Image>();
+        bottomRight = wheel.transform.Find("BottomRight").GetComponent<Image>();
 
-    private void SwitchTool()
-    {
-        index -= 1;
-        if (index == -1) {
-            index = 4;
-        }
-        if (index == 0) { mode = ToolModes.defaultMode; }
-        if (index == 1) { mode = ToolModes.seedsMode; }
-        if (index == 2) { mode = ToolModes.buildingMode; }
-        if (index == 3) { mode = ToolModes.hoeMode; }
-        if (index == 4) { mode = ToolModes.swordMode; }
+        currentImage = null;
+        selectedImage = null;
+
+        blue = new Color32(182, 205, 242, 190);
+        darkBlue = new Color32(34, 90, 200, 190);
+        white = new Color32(255, 255, 255, 190);
+        resetColors();
+
+        wheel.SetActive(false);
     }
 
     public ToolModes GetMode()
     {
         return mode;
     }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            wheel.SetActive(true);
+        }
+        else if (Input.GetKeyUp("tab"))
+        {
+            wheel.SetActive(false);
+            if (currentImage == topLeft)
+            {
+                mode = ToolModes.buildingMode;
+            }
+            else if (currentImage == topRight)
+            {
+                mode = ToolModes.farmMode;
+            }
+            else if (currentImage == bottomLeft)
+            {
+                mode = ToolModes.combatMode;
+            }
+            else if (currentImage == bottomRight)
+            {
+                mode = ToolModes.defaultMode;
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            Vector2 viewportPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition);  //convert game object position to VievportPoint
+
+            resetColors();
+            if (viewportPoint.x > 0.5f && viewportPoint.y > 0.5f) {
+                currentImage = topRight;
+            } else if (viewportPoint.x < 0.5f && viewportPoint.y > 0.5f) {
+                currentImage = topLeft;
+            } else if (viewportPoint.x > 0.5f && viewportPoint.y < 0.5f) {
+                currentImage = bottomRight;
+            } else if (viewportPoint.x < 0.5f && viewportPoint.y < 0.5f) {
+                currentImage = bottomLeft;
+            } else {
+                currentImage = null;
+            }
+
+            if (currentImage != null) {
+                currentImage.color = blue;
+            }
+            if (selectedImage != null)
+            {
+                selectedImage.color = darkBlue;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                selectedImage = currentImage;
+            }
+        }
+    }
+
+    private void resetColors()
+    {
+        topLeft.color = white;
+        topRight.color = white;
+        bottomLeft.color = white;
+        bottomRight.color = white;
+        if (selectedImage != null)
+        {
+            selectedImage.color = darkBlue;
+        }
+    }
 }
 
 public enum ToolModes
 {
     defaultMode,
-    seedsMode,
     buildingMode,
-    hoeMode,
-    swordMode
+    farmMode,
+    combatMode
 }
