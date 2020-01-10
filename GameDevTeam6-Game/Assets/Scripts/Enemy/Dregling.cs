518cd72f7e09820d412a7794ae9f0b0e7fbc646c
clarchiu <clarchiu@gmail.com>
2020-01-09 21:12:48 -0800

    some updates on enemy

---
+++ GameDevTeam6-Game/Assets/Scripts/Enemy/Dregling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class Dregling : Enemy
+{
+    //target should implement targetable interface
+    protected override void Attack(GameObject target) 
+    {
+        //throw new NotImplementedException();
+    }
+
+    protected override void InitializeProperties()
+    {
+        speed = 3f;
+        health = 50;
+        damage = 10;
+        preferredTarget = "Player";
+    }
+
+
+    void Start()
+    {
+        InitializeProperties();
+    }
+
+    void Update()
+    {
+        FollowTarget();
+    }
+
+}
---
+++ GameDevTeam6-Game/Assets/Scripts/Enemy/EnemyRange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour
+{
+    private Enemy parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<Dregling>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            parent.Target = collision.gameObject;
+            //print("target found");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            parent.Target = null;
+            //print("target found");
+        }
+    }
+
+}
