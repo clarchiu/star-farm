db653fa8843ab57d2b2ddb9e830f8ad733491c3a
rrus <RandyHR2000@gmail.com>
2020-02-05 20:00:48 -0800

    Added item pickup

--- GameDevTeam6-Game/Assets/Scripts/Defense/Projectile.cs
+++ GameDevTeam6-Game/Assets/Scripts/Defense/Projectile.cs
@@ -8,7 +8,6 @@ public class Projectile : MonoBehaviour
     private Rigidbody2D rigidBody = null;
     //Config Params
     [SerializeField] private float projectileSpeed = 10f;
-    private GameObject player;
 
     private float lifeTime = 2;
     private float currentTime = 0;
@@ -17,8 +16,6 @@ public class Projectile : MonoBehaviour
 
     void Awake(){
         FindRigidBody();
-        player = GameObject.FindGameObjectWithTag("Player");
-        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponent<Collider2D>(), true);
     }
 
     private void FindRigidBody(){
--- GameDevTeam6-Game/Assets/Scripts/Movement/PlayerDirection_.cs
+++ GameDevTeam6-Game/Assets/Scripts/Movement/PlayerDirection_.cs
@@ -17,39 +17,9 @@ public class PlayerDirection_ : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") == -1) {
-            anim.speed = 1;
-            direction = playerDir.left;
-            anim.SetFloat("Horizontal", -1);
-            anim.SetFloat("Vertical", 0);
-            walking = true;
-        } else if (Input.GetAxisRaw("Horizontal") == 1) {
-            anim.speed = 1;
-            direction = playerDir.right;
-            anim.SetFloat("Horizontal", 1);
-            anim.SetFloat("Vertical", 0);
-            walking = true;
-        } else if (Input.GetAxisRaw("Vertical") == -1)
-        {
-            anim.speed = 1;
-            direction = playerDir.down;
-            anim.SetFloat("Vertical", -1);
-            anim.SetFloat("Horizontal", 0);
-            walking = true;
-        } else if (Input.GetAxisRaw("Vertical") == 1)
-        {
-            anim.speed = 1;
-            direction = playerDir.up;
-            anim.SetFloat("Vertical", 1);
-            anim.SetFloat("Horizontal", 0);
-            walking = true;
-        } else
-        {
-            if (walking == true) { 
-                anim.speed = 0;
-                walking = false;
-            }
-        }
+        Vector2 direction = GetComponent<Rigidbody2D>().velocity.normalized;
+        anim.SetFloat("Horizontal", direction.x);
+        anim.SetFloat("Vertical", direction.y);
     }
 
     public playerDir GetDirection() {
