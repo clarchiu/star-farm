bab5121210c47566fc376a785db02a4639970ab0
rrus <RandyHR2000@gmail.com>
2020-02-13 19:13:26 -0800

    Fixed Clearence's scripts

--- GameDevTeam6-Game/Assets/Scripts/Enemy/EnemyAI.cs
+++ GameDevTeam6-Game/Assets/Scripts/Enemy/EnemyAI.cs
@@ -14,7 +14,43 @@ using Pathfinding;
 
 public class EnemyAI: MonoBehaviour
 {
+<<<<<<< Updated upstream
     private IState currentState;
+=======
+    #region Components
+    //GFX
+    private EnemyGFX gfx;
+    public EnemyGFX GFX { get => gfx; }
+
+    //Pathing
+    public AIPath aiPath;
+    private AIDestinationSetter destSetter;
+
+    //Physics
+    private Rigidbody2D rb;
+    public Rigidbody2D RB { get => rb; }
+
+    //Combat
+    private HealthBar_ healthBar;
+    private CircleCollider2D attackRangeCollider;
+    #endregion
+
+    #region Public Properties
+    //Constants for enemy type
+    public EnemyAttributes MyAttributes;
+
+    //Set by attackRangeCollider
+    public bool InAttackRange { get; private set; } = false;
+
+    //Set target to null if not in aggro range
+    public bool TargetInAggroRange
+    {
+        get
+        {   //calculates whether target is within 5 units of enemy
+            return (target.transform.position - transform.position).sqrMagnitude <= 5 * 5;
+        }
+    }
+>>>>>>> Stashed changes
 
     private GameObject target;
     public GameObject Target
@@ -55,9 +91,105 @@ public class EnemyAI: MonoBehaviour
         currentState = newState;
         currentState.Enter(this);
     }
+<<<<<<< Updated upstream
+=======
+    #endregion
+
+    #region ITargetable Implementation
+    void ITargetable.SetHealth(int amount)
+    {
+        MyAttributes.currentHealth = amount;
+    }
+
+    void ITargetable.RemoveHealth(GameObject source, int amount)
+    {
+        if (MyAttributes.currentHealth - amount > 0)
+        {
+            MyAttributes.currentHealth -= amount;
+            healthBar.UpdateHealthBar((float) MyAttributes.currentHealth / MyAttributes.maxHealth);
+
+            if (!GameObject.ReferenceEquals(target, source))
+            {
+                if (source.GetComponent<ITargetable>() != null)
+                {
+                    Target = source;
+                }
+            }
+        }
+        else
+        {
+            MyAttributes.currentHealth = 0;
+            Destroy(gameObject);
+        }
+    }
+>>>>>>> Stashed changes
 
     public void SetDirection(Vector2 direction)
     {
+<<<<<<< Updated upstream
         GetComponentInChildren<EnemyGFX>().Direction = direction;
+=======
+        MyAttributes.currentHealth += amount;
+    }
+
+    void ITargetable.KnockBack(Vector2 origin, float amount)
+    {
+        Vector2 deltaPosition = ((Vector2) this.transform.position - origin).normalized * amount;
+
+        if (aiPath.canMove == true)
+        {
+            aiPath.Move(deltaPosition);
+        }
+        else
+        {
+            rb.AddForce(deltaPosition * amount * 1000f, ForceMode2D.Force);
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    private void SetupComponents()
+    {
+        healthBar = GetComponent<HealthBar_>();
+        destSetter = GetComponent<AIDestinationSetter>();
+        rb = GetComponent<Rigidbody2D>();
+        gfx = GetComponentInChildren<EnemyGFX>();
+        attackRangeCollider = GetComponent<CircleCollider2D>();
+
+        if (healthBar == null || destSetter == null || rb == null
+            || gfx == null || aiPath == null || attackRangeCollider == null)
+        {
+            this.gameObject.SetActive(false);
+            throw new System.Exception("missing components on enemy, add in inspector");
+        }
+
+        if (MyAttributes.currentHealth == 0 && MyAttributes.maxHealth == 0 && MyAttributes.speed == 0
+            && MyAttributes.attackCoolDown == 0 && MyAttributes.attackDamage == 0 && MyAttributes.attackRange == 0)
+        {
+            this.gameObject.SetActive(false);
+            throw new System.Exception("missing MyStats values on EnemyAI script in inspector");
+        }
+
+        aiPath.maxSpeed = MyAttributes.speed;
+        attackRangeCollider.radius = MyAttributes.attackRange;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GameObject.ReferenceEquals(target, collision.gameObject))
+        {
+            InAttackRange = true;
+            Debug.Log("enemy in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (GameObject.ReferenceEquals(target, collision.gameObject))
+        {
+            InAttackRange = false;
+            Debug.Log("enemy out of range");
+        }
+>>>>>>> Stashed changes
     }
 }
--- GameDevTeam6-Game/Assets/Scripts/Enemy/States/AttackState.cs
+++ GameDevTeam6-Game/Assets/Scripts/Enemy/States/AttackState.cs
@@ -15,6 +15,22 @@ public class AttackState : IState
 
     void IState.Update()
     {
+<<<<<<< Updated upstream
         throw new NotImplementedException();
+=======
+        isAttacking = true;
+        attackCoolDown = parent.MyAttributes.attackCoolDown; 
+
+        parent.GFX.MyAnimator.SetTrigger("attack");
+
+        targetable.RemoveHealth(parent.gameObject, parent.MyAttributes.attackDamage); 
+        targetable.KnockBack(parent.transform.position, 50f); //TODO: make amount of knockback scale with damage?
+
+        yield return new WaitForSeconds(parent.GFX.MyAnimator.GetCurrentAnimatorStateInfo(2).length); //check how long the animation is
+
+        isAttacking = false;
+
+        yield break;
+>>>>>>> Stashed changes
     }
 }
