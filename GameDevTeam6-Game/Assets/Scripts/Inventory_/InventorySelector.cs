dbd3129f9cbe6a23589a3824bf94a67f8c0e1152
Kyle <kylevanwinkoop@gmail.com>
2020-03-04 20:26:00 -0800

    New scene, invintory updates
    Made a scene for me to work in.
    Added a invintory button method

---
+++ GameDevTeam6-Game/Assets/Scripts/Inventory_/InventorySelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class InventorySelector: MonoBehaviour
+{
+    public GameObject button1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       // Button btn = GameOjbect.GetComponent<Button>();
+       // btn.onClick().AddListener(TaskOnClick);
+
+
+    }
+
+   public void TaskOnClick(int buttonNum)
+    {
+        if (1 == Inventory_mineral.Instance.items.Count)
+        {
+            Inventory_mineral.Instance.items[buttonNum].GetMineralType();
+            Debug.Log("You h clicked the button!");
+            Debug.Log(Inventory_mineral.Instance.items[buttonNum].GetMineralType().ToString());
+        }
+    }
+}
+
+// Update is called once per frame
